7e71b15c74a60371e88857e1b22841b72abc1295
mariel <zivy.mma@gmail.com>
2026-04-21 18:04:43 -0600

    1er proyecto

---
+++ tipos-de-datos.js
@@ -0,0 +1,10 @@
+console.log(typeof 42);
+console.log(typeof 'Veinticinco');
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof "");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof false);
+console.log(typeof "*");
